a8fe0dcf78513105f4479a5a07eeadc204243535
Jakub Pawlik <jakpaw5@st.ext.amu.edu.pl>
2023-06-12 02:24:23 +0200

    Sprawdzenie wejścia, można zaczynać

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
-#include <iostream>
+#include "map_functions.h"
 
-int main() {
-    std::cout << "Whatever" << std::endl;
+int main()
+{
+    vector < int > multi_a;
+    vector < int > not_used;
+    vector < int > restriction_map;
+    load_vector(multi_a);
+    copy(multi_a.begin(), multi_a.end(), back_inserter(not_used));
+
+    int site_count = check_size(multi_a.size()); //ilosc przeciec obliczona ze wzoru
+    if(!site_count) {
+        cout << "\nNieprawidlowy rozmiar instancji";
+        return 101;
+    }
+
+//    display_vector(not_used);
+    if (!check_first_element(multi_a, not_used, restriction_map)) {
+        cout << "\nBrak pierwszego elementu w zbiorze";
+        return 202;
+    }
+//    display_vector(not_used);
+//    display_vector(restriction_map);
+    cout << "\nInstancja ma prawidlowy rozmiar, oraz pierwszy element mapy znajduje sie w multizbiorze";
+
+    cout << "\nSkladanie mapy dla multizbioru:\n";
+    display_vector(multi_a);
+
+    int found = 0;
+
+    seek(multi_a, not_used, restriction_map, site_count, &found);
+
+    if (!found) {
+        cout << "\nBrak rozwiazania";
+    }
+
     return 0;
 }
--- map_functions.h
+++ map_functions.h
@@ -1,38 +1,98 @@
-//
-// Created by kubah on 10.06.2023.
-//
-
 #ifndef AKWB4_MAP_FUNCTIONS_H
 #define AKWB4_MAP_FUNCTIONS_H
 
-void szukaj(...,int ind,int *jest)
-{
-...
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <cmath>
 
-if (ind==maxind)
-{
-<wypisz rozwiązanie>
-*jest=1;
-}
-else for (...)
-{
-...
-if (<warto kontynuować bieżące rozwiązanie>)
-szukaj(...,ind+1,jest);
-if (*jest==1) break;
-...
-}
+using namespace std;
+
+string instance = "test";
+
+void load_vector(vector<int> &wektor){
+    ifstream fh("instances/"+instance+".txt");
+    string line;
+
+    while (getline(fh, line))
+    {
+        istringstream row(line);
+        int val;
+        while (row >> val)
+        {
+            wektor.push_back(val);
+        }
+    }
+
+    fh.close();
 }
 
-int main(...)
-{
-    ...
-    int jest;
+void display_vector(vector < int > &wektor){
+    cout << "\n";
+    for(int x: wektor)
+        cout << x << " ";
+}
 
-    ...
-    szukaj(...,0,&jest);
-    if (jest==0) {<poinformuj o braku rozwiązania>}
-    ...
+int check_size(int a_size){
+    double k = (-3 + sqrt(8*a_size + 1)) / 2;
+    if (k == (int)k)
+        return (int)k;
+    else
+        return 0;
+}
+
+bool find_element(int key, vector < int > v) {
+    if (find(v.begin(), v.end(), key) != v.end())
+        return true;
+    else
+        return false;
+}
+
+void delete_element(int key, vector < int > &v) {
+    for(int pos = 0; pos < v.size(); pos++)
+    {
+        if (v[pos] == key) {
+            v.erase(begin(v) + pos);
+            break;
+        }
+    }
+}
+
+bool check_first_element(vector < int > &multi_a, vector < int > &not_used, vector < int > &restriction_map){
+    int max = 0, nd_max = 0;
+    for (int e: multi_a){
+        if (e > max) {
+            nd_max = max;
+            max = e;
+        }
+    }
+    //cout <<"\nMax: " << max << " 2nd_Max: " << nd_max;
+    int first = max - nd_max;
+    //cout << "\n" << first;
+    restriction_map.push_back(first);
+
+
+    delete_element(first, not_used);
+    delete_element(nd_max, not_used);
+
+    return find_element(first, multi_a);
+}
+
+
+
+void seek(vector < int > &multi_a, vector < int > &not_used, vector <int> &restriction_map, int maxInd, int *found) {
+    if (restriction_map.size() == maxInd) {
+        display_vector(restriction_map);
+        *found = 1;
+    }
+    else for (int frag: not_used){
+        cout << "\t" << frag;
+
+        if (*found==1)
+            break;
+        }
+
 }
 
 #endif //AKWB4_MAP_FUNCTIONS_H
